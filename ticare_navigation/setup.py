575fc622f591a3c500674e4bdf355a67a64bb24a
Catalina Mor'an <ketamoran@gmail.com>
2026-03-16 13:55:24 +0100

    Inicializar paquete navigation

---
+++ ticare_navigation/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'ticare_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='catalina',
+    maintainer_email='ketamoran@gmail.com',
+    description='Implementación del stack de navegación para el proyecto TiCare',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
