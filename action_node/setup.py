afeda7ee80dd0a2a62d22c1081546faaaa6c917b
Catalina Mor'an <ketamoran@gmail.com>
2026-03-24 00:45:20 +0100

    Inicializar branch del Bittle

---
+++ action_node/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'action_node'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='catalina',
+    maintainer_email='ketamoran@gmail.com',
+    description='Implementación del nodo de acciones de Bittle',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
