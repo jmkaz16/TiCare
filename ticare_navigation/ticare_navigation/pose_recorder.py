c979d70c8377694ab6a14ee2c6a49e18a61cb0e3
Catalina Mor'an <ketamoran@gmail.com>
2026-04-07 11:22:02 +0200

    Añadir al nodo PoseRecorder la suscripción al tópico /amcl_pose y guardar las posiciones en la carpeta data

--- ticare_navigation/ticare_navigation/pose_recorder.py
+++ ticare_navigation/ticare_navigation/pose_recorder.py
@@ -1,20 +1,71 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node 
 
 from ticare_interfaces.srv import SavePose
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+from rosidl_runtime_py.convert import message_to_yaml
 
 class PoseRecorder(Node):
 
     def __init__(self):
         super().__init__('pose_recorder')
+        
+        self.current_pose = None # Initialize current_pose to None to handle possible crashes at startup
+
         self.srv = self.create_service(SavePose, 'save_pose', self.save_pose_callback)
+        
+        self.subscription = self.create_subscription(
+            PoseWithCovarianceStamped,
+            'amcl_pose',
+            self.pose_callback,
+            10
+        )
+        self.subscription  # prevent unused variable warning
 
+        # Obtain the base path to the installed package
+        package_share_path = get_package_share_directory('ticare_navigation')
+
+        # Save the path of the 'data' folder as a variable to avoid recalculating it eveery time a position is saved
+        self.data_dir = os.path.join(package_share_path, 'data')
+
+        if not os.path.exists(self.data_dir):  # In case the 'data' folder doesn't exist
+            os.makedirs(self.data_dir)
+
+    def pose_callback(self, msg):
+        self.current_pose = msg
+        
     def save_pose_callback(self, request, response):
-        response.success = True
-        response.message = f'Pose saved successfully: {request.label}'
-        self.get_logger().info(f'Incoming request: {request.label}')
+        if self.current_pose is None:
+            response.success = False
+            response.message = 'No pose data available to save.'
+            self.get_logger().warn('Attempted to save pose, but no pose data is available.')
+            
+            return response
 
-        return response
+        file_name = f'{request.label}.yaml'
+        file_path = os.path.join(self.data_dir, file_name)
+
+        try:
+            # Save the current pose to a YAML file
+            with open(file_path, 'w') as file:
+                file.write(message_to_yaml(self.current_pose))
+            
+            response.success = True
+            response.message = f'Pose saved successfully: {request.label}'
+            self.get_logger().info(f'Incoming request: {request.label}')
+
+            return response
+        
+        except Exception as e:
+            response.success = False
+            response.message = f'Error occurred while saving pose: {str(e)}'
+            self.get_logger().error(response.message)
+            
+            return response
 
 # ros2 service call /save_pose ticare_interfaces/srv/SavePose "{label: 'juan'}"
 
@@ -23,11 +74,15 @@ def main():
 
     pose_recorder = PoseRecorder()
 
-    rclpy.spin(pose_recorder)
-
-    pose_recorder.destroy_node()
-
-    rclpy.shutdown()
+    try:
+        rclpy.spin(pose_recorder)
+    
+    except KeyboardInterrupt:
+        pass
+    
+    finally:
+        pose_recorder.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
