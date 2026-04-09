a0be907d932c9d363149724ef81da0207ad14d0b
jmkaz16 <jmkaz16@gmail.com>
2026-04-09 23:50:39 +0200

    Renombrar e implementar audio_processor.py como nodo ROS

--- bittle_communication/setup.py
+++ bittle_communication/setup.py
@@ -1,6 +1,3 @@
-import os
-from glob import glob
-
 from setuptools import find_packages, setup
 
 package_name = "bittle_communication"
@@ -12,7 +9,6 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name, "state"), glob("state/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -28,6 +24,7 @@ setup(
     entry_points={
         "console_scripts": [
             "communication_publisher = bittle_communication.communication_publisher:main",
+            "audio_processor = bittle_communication.audio_processor:main",
         ],
     },
 )
