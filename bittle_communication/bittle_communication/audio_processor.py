e3ef643fe4657167c55ebafef9caf1885c19729c
jmkaz16 <jmkaz16@gmail.com>
2026-04-10 00:38:08 +0200

    Revert "Reorganizar rutas para ROS"
    This reverts commit b7cac8ce805497d20ea252db7ff4604225b1499f.

--- bittle_communication/bittle_communication/audio_processor.py
+++ bittle_communication/bittle_communication/audio_processor.py
@@ -5,11 +5,17 @@ from typing import List, Optional
 import rclpy
 from rclpy.node import Node
 
-from bittle_communication.stt.record_audio import record_audio
-from bittle_communication.stt.whisper_stt import transcribe_audio
-from bittle_communication.stt.wake_word import listen_for_wake_word
-from bittle_communication.stt.tiago_spacy import parse_command
-from bittle_communication.stt.semantic_chunk import semantic_chunk
+from stt.record_audio import record_audio
+from stt.whisper_stt import transcribe_audio
+from stt.wake_word import listen_for_wake_word
+from stt.tiago_spacy import parse_command
+from stt.semantic_chunk import semantic_chunk
+
+from ament_index_python.packages import get_package_share_directory
+
+# -------------------------
+# Configuración / utilidades
+# -------------------------
 
 
 class AudioProcessor(Node):
@@ -20,7 +26,6 @@ class AudioProcessor(Node):
         self.declare_parameter("recording_duration", 5)  # duración de la grabación en segundos
 
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.get_logger().info(f"AudioProcessor iniciado. Current directory: {self.current_dir}")
         self.state_dir = os.path.join(self.current_dir, "..", "state")
 
         self.audio_path = os.path.join(self.state_dir, "audio.wav")
