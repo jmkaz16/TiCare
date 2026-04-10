437c9f2630d3a04cf181c89ca0efdd23ddffa677
Catalina Mor'an <ketamoran@gmail.com>
2026-04-10 11:55:44 +0200

    Cambiar la conexión por bluetooth a un socket de python y pasar el MAC como parámetro

--- bittle_actions/bittle_actions/action_commands.py
+++ bittle_actions/bittle_actions/action_commands.py
@@ -5,8 +5,9 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
-import serial
-import time
+# import serial
+import socket
+# import time
 
 '''BITTLE_COMMANDS = {
     "stand": "kup",
@@ -56,8 +57,15 @@ class BittleAction(Node):
 
     def __init__(self):
         super().__init__('bittle_action')
-        self.ser = serial.Serial('/dev/rfcomm0', 115200, timeout=1)
-        time.sleep(2)
+        
+        self.declare_parameter('MAC_ADDRESS', '00:00:00:00:00:AA') # declare a parameter with a default empty value
+        self.MAC = self.get_parameter('MAC_ADDRESS').get_parameter_value().string_value
+        self.get_logger().info(f'Configuring Bittle with MAC address: {self.MAC}')
+        
+        # self.ser = serial.Serial('/dev/rfcomm0', 115200, timeout=1)
+        self.ser = None
+        self.connect_serial()
+        
         self.subscription = self.create_subscription(
             String,
             '/bittle_cmd',
@@ -65,12 +73,26 @@ class BittleAction(Node):
             10)
         self.subscription  # prevent unused variable warning
 
+    def connect_serial(self, PORT=1):
+        try:
+            self.ser = socket.socket(
+                socket.AF_BLUETOOTH,
+                socket.SOCK_STREAM,
+                socket.BTPROTO_RFCOMM
+            )
+            self.ser.connect((self.MAC, PORT))
+            self.get_logger().info(f'Connected to Bittle {self.MAC}!')
+        except Exception as e:
+            self.get_logger().error(f'Failed to connect to Bittle: {e}')
+
     def execute_command(self, msg):
+        if self.ser is None:
+            self.get_logger().error('Serial connection not established')
+            return
         # cmd = BITTLE_COMMANDS.get(msg.data) con diccionario
         cmd = "k" + msg.data
         if cmd:
-            self.ser.write(f'{cmd}\n'.encode())
-            self.ser.flush() # forces transmisssion from OS serial write buffer to Bittle
+            self.ser.send(f'{cmd}\n'.encode())
             self.get_logger().info(f'Sent:{cmd}')
         else:
             self.get_logger().warn('Unknown command')
