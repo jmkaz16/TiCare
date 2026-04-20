1698cc09a5075a6c0d6a9860bd5448c2135705bc
jmkaz16 <jmkaz16@gmail.com>
2026-04-20 12:22:57 +0200

    Configurar paquete para incluir archivos launch

--- ticare_navigation/setup.py
+++ ticare_navigation/setup.py
@@ -1,32 +1,35 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 from glob import glob
 
-package_name = 'ticare_navigation'
+package_name = "ticare_navigation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/data', glob('data/*.yaml')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/data", glob("data/*.yaml")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='catalina',
-    maintainer_email='ketamoran@gmail.com',
-    description='Implementación del stack de navegación para el proyecto TiCare',
-    license='Apache-2.0',
+    maintainer="catalina",
+    maintainer_email="ketamoran@gmail.com",
+    description="Implementación del stack de navegación para el proyecto TiCare",
+    license="Apache-2.0",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'pose_recorder = ticare_navigation.pose_recorder:main',
+        "console_scripts": [
+            "pose_recorder = ticare_navigation.pose_recorder:main",
         ],
     },
 )
