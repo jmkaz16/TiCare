bbb200fe3ff7e3c2fb961b2d792a0234d6ec1f7c
Mario976-boop <mario.guerra.castelo@alumnos.upm.es>
2026-04-21 18:46:02 +0200

    Se evita que guarden los archivos order, audio y topic

--- bittle_communication/bittle_communication/stt/whisper_stt.py
+++ bittle_communication/bittle_communication/stt/whisper_stt.py
@@ -1,13 +1,12 @@
+# stt/whisper_stt.py
+
 import whisper
 
-
-def transcribe_audio(audio_path):
+def transcribe_audio(audio_data):
     print("Cargando modelo Whisper...")
     model = whisper.load_model("base", device="cpu")
-
     print("Transcribiendo audio...")
-    result = model.transcribe(audio_path, language="es")
-
+    result = model.transcribe(audio_data, language="es")
     text = result["text"]
     print("Transcripción:", text)
     return text
