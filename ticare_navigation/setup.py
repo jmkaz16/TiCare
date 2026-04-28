53a8ff47f0df042943133e6267ece3fddcddf589
Lucas <l.goni@alumnos.upm.es>
2026-04-28 18:00:37 +0200

    Add the .txt files to the setup.py so point_selection.txt is accessible

--- ticare_navigation/setup.py
+++ ticare_navigation/setup.py
@@ -14,6 +14,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "data"), glob("data/*.yaml")),
+        (os.path.join("share", package_name, "data"), glob("data/*.txt")),
         (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*.world")),
