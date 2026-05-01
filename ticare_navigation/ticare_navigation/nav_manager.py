70ab99e6e1136d82f68afd01a1d00e9c81e87566
Catalina Mor'an <ketamoran@gmail.com>
2026-05-01 22:20:08 +0200

    Complete TODO functions

--- ticare_navigation/ticare_navigation/nav_manager.py
+++ ticare_navigation/ticare_navigation/nav_manager.py
@@ -1,3 +1,6 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
@@ -5,9 +8,13 @@ from rclpy.action import ActionClient
 from enum import Enum
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
 from ticare_interfaces.srv import SavePose
 from nav2_msgs.action import NavigateToPose
 from nav2_msgs.action import FollowWaypoints
+
+import yaml
+from rosidl_runtime_py.convert import yaml_to_msg
 
 
 class NavigationState(Enum):
@@ -16,7 +23,7 @@ class NavigationState(Enum):
 
     Attributes:
         IDLE: System is initialized and waiting for a mission.
-        LOCALIZING: Checks if a recovery rotation is needed to improve AMCL localization before 
+        LOCALIZING: Checks if a recovery rotation is needed to improve AMCL localization before
         proceeding.
         SAVING_START_POSE: Records the starting position for the "Returning Home" phase.
         SEARCHING: Executes the search pattern in the target room.
@@ -36,18 +43,20 @@ class NavigationState(Enum):
     AWAITING_RETURN = 7
     NAV_TO_OBJECT = 8
 
+
 class VisionCommand(Enum):
     """
     Defines the vision commands sent from NavManager to the ticare_vision package.
 
     Attributes:
-        START_VIS: The robot has localized itself and activates the camera to start the search 
+        START_VIS: The robot has localized itself and activates the camera to start the search
         protocol.
         STOP_VIS: The search protocol has finished, deactivate the camera.
     """
-    
+
     START_VIS = "start_vis"
     STOP_VIS = "stop_vis"
+
 
 class ComStatus(Enum):
     """
@@ -61,6 +70,7 @@ class ComStatus(Enum):
     HOME = "home"
     OBJECT_POINT = "object_point"
 
+
 class SavePoseLabel(Enum):
     """
     Defines the labels for the poses saved by the SavePose service.
@@ -72,6 +82,7 @@ class SavePoseLabel(Enum):
 
     START_POSE = "start_pose"
     OBJECT_POSE = "object_pose"
+
 
 class NavManager(Node):
     """
@@ -95,6 +106,13 @@ class NavManager(Node):
 
         nav_to_pose_client (ActionClient): Sends the robot to the target using A* algorithm.
         follow_waypoints_client (ActionClient): Executes coverage path planning.
+
+        timer (Timer): Main control loop timer that checks the current state and executes the
+        corresponding behavior.
+
+        data_dir (str): The directory path where the YAML files of the initial and object poses are
+        saved.
+        config_dir (str): The directory path where the YAML file of the coverage points is saved.
     """
 
     def __init__(self) -> None:
@@ -102,19 +120,18 @@ class NavManager(Node):
         clients and timers."""
         super().__init__("nav_manager")
 
-        self.state : NavigationState = NavigationState.IDLE
-        self.control_loop_period : float = 0.1 # [s] Period for the main control loop timer
+        self.state: NavigationState = NavigationState.IDLE
+        self.control_loop_period: float = 0.1  # [s] Period for the main control loop timer
 
-        self.recovery_rotation_active : bool = False # Flag to indicate if recovery rotation needed
-        self.recovery_rotation_duration : float = 30.0 # [s] Duration for the recovery rotation
-        self.recovery_rotation_start_time : float = 0.0 # [s] Timestamp when the recovery starts
-        self.recovery_rotation_speed : float = 0.5 # [rad/s] Angular speed during recovery rotation
+        self.recovery_rotation_active: bool = False  # Flag to indicate if recovery rotation needed
+        self.recovery_rotation_duration: float = 30.0  # [s] Duration for the recovery rotation
+        self.recovery_rotation_start_time: float = 0.0  # [s] Timestamp when the recovery starts
+        self.recovery_rotation_speed: float = 0.5  # [rad/s] Angular speed during recovery rotation
 
-        self.object_detected : bool = False # Flag to track if the object has been detected 
+        self.object_detected: bool = False  # Flag to track if the object has been detected
 
-        self.search_duration : float = 300.0 # [s] Max duration for the search phase
-        self.search_start_time : float = 0.0 # [s] Timestamp when the search phase starts
-
+        self.search_duration: float = 300.0  # [s] Max duration for the search phase
+        self.search_start_time: float = 0.0  # [s] Timestamp when the search phase starts
 
         self.vision_pub = self.create_publisher(String, "nav2vis", 10)
         self.communication_pub = self.create_publisher(String, "nav2com", 10)
@@ -133,7 +150,17 @@ class NavManager(Node):
         self.timer = self.create_timer(self.control_loop_period, self.control_loop_callback)
 
         self.get_logger().info(f"Navigation Manager initialized. Current state: {self.state.name}")
-    
+
+        package_share_path = get_package_share_directory("ticare_navigation")
+
+        self.data_dir = os.path.join(package_share_path, "data")
+        if not os.path.exists(self.data_dir):
+            os.makedirs(self.data_dir)
+
+        self.config_dir = os.path.join(package_share_path, "config")
+        if not os.path.exists(self.config_dir):
+            os.makedirs(self.config_dir)
+
     def control_loop_callback(self) -> None:
         """Main control loop that orchestrates FSM transitions based on the current state."""
         match self.state:
@@ -145,9 +172,9 @@ class NavManager(Node):
                     self.perform_recovery_rotation()
 
                 else:
-                    self.send_save_pose_request(SavePoseLabel.START_POSE)
                     self.get_logger().info("Moving to SAVING_START_POSE.")
                     self.state = NavigationState.SAVING_START_POSE
+                    self.send_save_pose_request(SavePoseLabel.START_POSE)
 
             case NavigationState.SAVING_START_POSE:
                 pass
@@ -163,32 +190,39 @@ class NavManager(Node):
 
                     self.get_logger().info("Search duration exceeded: Moving to RETURNING_HOME.")
                     self.state = NavigationState.RETURNING_HOME
-                    
-                # TODO: Implement cancel active search goal and send home goal
+                    self.send_nav_to_pose_goal()
+
+                if self.object_detected:
+
+                    self.get_logger().info(
+                        "Object detected during search: Moving to SAVING_OBJECT_POSE."
+                    )
+                    self.state = NavigationState.SAVING_OBJECT_POSE
+                    self.send_save_pose_request(SavePoseLabel.OBJECT_POSE)
 
             case NavigationState.SAVING_OBJECT_POSE:
                 pass
 
             case NavigationState.RETURNING_HOME:
                 pass
-            
+
             case NavigationState.AWAITING_RETURN:
                 pass
 
             case NavigationState.NAV_TO_OBJECT:
                 pass
-    
+
     def perform_recovery_rotation(self) -> None:
         """Executes a recovery rotation to improve AMCL localization."""
         now = self.get_clock().now()
-    
+
         if self.recovery_rotation_start_time == 0.0:
             self.recovery_rotation_start_time = now
             self.get_logger().info("Starting recovery rotation for localization.")
             return
 
         elapsed = (now - self.recovery_rotation_start_time).nanoseconds / 1e9
-        
+
         if elapsed < (self.recovery_rotation_duration / 2):
             self.publish_cmd_vel_msg(0.0, self.recovery_rotation_speed)
 
@@ -199,10 +233,11 @@ class NavManager(Node):
             self.publish_cmd_vel_msg(0.0, 0.0)
             self.recovery_rotation_active = False
             self.recovery_rotation_start_time = 0.0
-            
+
             self.get_logger().info("Recovery rotation completed. Moving to SAVING_START_POSE.")
             self.state = NavigationState.SAVING_START_POSE
-    
+            self.send_save_pose_request(SavePoseLabel.START_POSE)
+
     def publish_cmd_vel_msg(self, linear_x: float, angular_z: float) -> None:
         """Publishes a Twist message to the cmd_vel topic.
 
@@ -214,7 +249,7 @@ class NavManager(Node):
         msg.linear.x = linear_x
         msg.angular.z = angular_z
         self.cmd_vel_pub.publish(msg)
-    
+
     def publish_vision_command(self, command: VisionCommand) -> None:
         """Publishes a vision command to the ticare_vision package.
 
@@ -241,13 +276,68 @@ class NavManager(Node):
         """Sends a service request to save the current pose with the specified label.
 
         Args:
-            label (SavePoseLabel): The label for the pose to be saved (e.g., START_POSE, 
+            label (SavePoseLabel): The label for the pose to be saved (e.g., START_POSE,
             OBJECT_POSE).
         """
+        if not self.save_pose_client.service_is_ready():
+            self.get_logger().warning("SavePose service is not available.")
+            return None
 
-        # TODO: Implement service request to save the current pose with the specified label
+        match label:
+            case SavePoseLabel.START_POSE:
+                self.get_logger().info("Requesting to save START_POSE.")
 
-    # TODO: Implement additional helper methods for sending navigation goals, etc.
+            case SavePoseLabel.OBJECT_POSE:
+                self.get_logger().info("Requesting to save OBJECT_POSE.")
+
+            case _:
+                self.get_logger().warning(f"Unknown SavePose label: {label}")
+                return None
+
+        future = self.save_pose_client.call_async(SavePose.Request(label=label.value))
+        future.add_done_callback(self.save_pose_response_callback)
+
+    def send_nav_to_pose_goal(self) -> None:
+        """Sends a goal to the NavigateToPose action server to navigate to the specified pose."""
+        match self.state:
+            case NavigationState.RETURNING_HOME:
+                self.get_logger().info("Sending NavigateToPose goal to return home.")
+                file_name = os.path.join(self.data_dir, f"{SavePoseLabel.START_POSE.value}.yaml")
+
+            case NavigationState.NAV_TO_OBJECT:
+                self.get_logger().info("Sending NavigateToPose goal to navigate to object.")
+                file_name = os.path.join(self.data_dir, f"{SavePoseLabel.OBJECT_POSE.value}.yaml")
+
+        with open(file_name, "r") as file:
+            yaml_data = yaml.safe_load(file)
+
+        goal_pose = yaml_to_msg(yaml_data, PoseStamped)
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose = goal_pose
+
+        self.nav_to_pose_client.wait_for_server()
+        future = self.nav_to_pose_client.send_goal_async(
+            goal_msg, feedback_callback=self.nav_to_pose_feedback_callback
+        )
+        future.add_done_callback(self.nav_to_pose_response_callback)
+
+    def send_follow_waypoints_goal(self) -> None:
+        """Sends a goal to the FollowWaypoints action server to execute a coverage path."""
+        file_name = os.path.join(self.config_dir, "coverage_points.yaml")
+
+        with open(file_name, "r") as file:
+            yaml_data = yaml.safe_load(file)
+
+        waypoints = [yaml_to_msg(point, PoseStamped) for point in yaml_data["waypoints"]]
+
+        goal_msg = FollowWaypoints.Goal()
+        goal_msg.poses = waypoints
+
+        self.follow_waypoints_client.wait_for_server()
+        future = self.follow_waypoints_client.send_goal_async(
+            goal_msg, feedback_callback=self.follow_waypoints_feedback_callback
+        )
+        future.add_done_callback(self.follow_waypoints_response_callback)
 
     def vision_callback(self, msg: String) -> None:
         """
@@ -290,24 +380,119 @@ class NavManager(Node):
             case _:
                 self.get_logger().warning(f"Unknown communication command: {command}")
 
-    # TODO: Implement service and action client callbacks 
     def save_pose_response_callback(self, future) -> None:
-        # TODO: If response is successful, publish "start_vis", "object_point" or "stop_vis" 
-        # depending on the service request label or current state
-        pass
+        """
+        Handles the response from the SavePose service.
+
+        Args:
+            future: The future object representing the service call.
+        """
+        try:
+            response = future.result()
+            self.get_logger().info(f"SavePose service response: {response.success}")
+
+            if response.success:
+                self.get_logger().info("Pose saved successfully.")
+
+                if self.state == NavigationState.SAVING_START_POSE:
+                    self.publish_vision_command(VisionCommand.START_VIS)
+                    self.search_start_time = self.get_clock().now()
+                    self.get_logger().info("Starting search: Moving to SEARCHING.")
+                    self.state = NavigationState.SEARCHING
+                    self.send_follow_waypoints_goal()
+
+                elif self.state == NavigationState.SAVING_OBJECT_POSE:
+                    self.publish_vision_command(VisionCommand.STOP_VIS)
+                    self.get_logger().info("Object pose saved: Moving to RETURNING_HOME.")
+                    self.state = NavigationState.RETURNING_HOME
+                    self.send_nav_to_pose_goal()
+
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
 
     def nav_to_pose_response_callback(self, future) -> None:
-        pass
+        """
+        Handles the response from the NavigateToPose action server.
+        Args:
+            future: The future object representing the action goal response.
+        """
+        nav_to_pose_goal_handle = future.result()
+        if not nav_to_pose_goal_handle.accepted:
+            self.get_logger().error("NavigateToPose goal was rejected.")
+            return
+
+        self.get_logger().info("NavigateToPose goal accepted.")
+
+        self.nav_to_pose_result_future = nav_to_pose_goal_handle.get_result_async()
+        self.nav_to_pose_result_future.add_done_callback(self.nav_to_pose_result_callback)
 
     def nav_to_pose_result_callback(self, future) -> None:
-        pass
+        """
+        Handles the result from the NavigateToPose action server.
+
+        Args:
+            future: The future object representing the action result.
+        """
+        result = future.result().result
+        self.get_logger().info(f"NavigateToPose result received: {result}")
+
+        if self.state == NavigationState.RETURNING_HOME:
+            self.publish_communication_status(ComStatus.HOME)
+            self.get_logger().info("Returned home: Moving to AWAITING_RETURN.")
+            self.state = NavigationState.AWAITING_RETURN
+
+        elif self.state == NavigationState.NAV_TO_OBJECT:
+            self.publish_communication_status(ComStatus.OBJECT_POINT)
+            self.get_logger().info("Arrived at object location.")
+            self.state = NavigationState.IDLE
+
+    def nav_to_pose_feedback_callback(self, feedback_msg) -> None:
+        """
+        Handles feedback from the NavigateToPose action server.
+
+        Args:
+            feedback_msg: The feedback message from the action server.
+        """
+        feedback = feedback_msg.feedback
+        self.get_logger().info(f"NavigateToPose feedback received: {feedback}")
 
     def follow_waypoints_response_callback(self, future) -> None:
-        pass
+        """
+        Handles the response from the FollowWaypoints action server.
+
+        Args:
+            future: The future object representing the action goal response.
+        """
+        follow_waypoints_goal_handle = future.result()
+        if not follow_waypoints_goal_handle.accepted:
+            self.get_logger().error("FollowWaypoints goal was rejected.")
+            return
+
+        self.get_logger().info("FollowWaypoints goal accepted.")
+
+        self.follow_waypoints_result_future = follow_waypoints_goal_handle.get_result_async()
+        self.follow_waypoints_result_future.add_done_callback(self.follow_waypoints_result_callback)
 
     def follow_waypoints_result_callback(self, future) -> None:
-        pass
-    
+        """
+        Handles the result from the FollowWaypoints action server.
+
+        Args:
+            future: The future object representing the action result.
+        """
+        result = future.result().result
+        self.get_logger().info(f"FollowWaypoints result received: {result}")
+
+    def follow_waypoints_feedback_callback(self, feedback_msg) -> None:
+        """
+        Handles feedback from the FollowWaypoints action server.
+
+        Args:
+            feedback_msg: The feedback message from the action server.
+        """
+        feedback = feedback_msg.feedback
+        self.get_logger().info(f"FollowWaypoints feedback received: {feedback}")
+
 
 def main(args=None) -> None:
     """Main function to initialize the NavManager node and start spinning."""
@@ -315,7 +500,7 @@ def main(args=None) -> None:
     nav_manager = NavManager()
 
     try:
-        rclpy.spin(nav_manager)
+        rclpy.spin()
 
     except KeyboardInterrupt:
         pass
