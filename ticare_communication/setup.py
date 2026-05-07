0a3d0b5c7fbc6a93f067540880b851e14056c45c
Catalina Mor'an <ketamoran@gmail.com>
2026-05-07 23:07:58 +0200

    Add console scripts for audio_saver.py and lanza_subprocess.py

--- ticare_communication/setup.py
+++ ticare_communication/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "ticare_communication"
@@ -9,7 +12,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch", ["launch/communication_launch.py"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "data"), glob("data/*.wav")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -23,6 +27,8 @@ setup(
     entry_points={
         "console_scripts": [
             "state_manager = ticare_communication.state_manager:main",
+            "save_audio = ticare_communication.audio_saver:main",
+            "lanza_subprocess = ticare_communication.lanza_subprocess:main",
         ],
     },
 )
