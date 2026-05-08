749ebe2c809890cd8b197f29f4620c763537d9ad
Catalina Mor'an <ketamoran@gmail.com>
2026-05-08 17:14:29 +0200

    Add launch file

--- ticare_communication/launch/communication_launch.py
+++ ticare_communication/launch/communication_launch.py
@@ -3,14 +3,17 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="ticare_communication",
-                executable="state_manager",
-                name="state_manager",
-                output="screen",
-                emulate_tty=True,
-            )
-        ]
+    capture_audio_node = Node(
+        package="audio_capture",
+        executable="audio_capture_node",
+        output="screen",
+        parameters=[{"format": "wave"}],
     )
+
+    communication_node = Node(
+        package="ticare_communication",
+        executable="state_manager",
+        output="screen",
+    )
+
+    return LaunchDescription([capture_audio_node, communication_node])
