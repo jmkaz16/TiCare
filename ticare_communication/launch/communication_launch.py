96ac55875f1fd72eb0a3a0020a6700ee2b6b5366
Lucas <l.goni@alumnos.upm.es>
2026-05-11 10:54:43 +0200

    Add use_sim_time True for the simulation

--- ticare_communication/launch/communication_launch.py
+++ ticare_communication/launch/communication_launch.py
@@ -1,19 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 
 def generate_launch_description():
+
+    use_sim_time = DeclareLaunchArgument(
+        "use_sim_time",
+        default="True",
+        description="Whether or not to use simulation time for the communication nodes.",
+    )
+
     capture_audio_node = Node(
         package="audio_capture",
         executable="audio_capture_node",
         output="screen",
-        parameters=[{"format": "wave"}],
+        parameters=[{"format": "wave", "use_sim_time": LaunchConfiguration("use_sim_time")}],
     )
 
     communication_node = Node(
         package="ticare_communication",
         executable="state_manager",
         output="screen",
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}],
     )
 
-    return LaunchDescription([capture_audio_node, communication_node])
+    return LaunchDescription([use_sim_time, capture_audio_node, communication_node])
